e310a6947b1233b0803092f345db3ecf4636827f
ScriptLineStudios <scriptlinestudios@protonmail.com>
2024-02-08 17:18:55 +0200

    huh

---
+++ include/chunk.h
@@ -0,0 +1,20 @@
+#ifndef ___CHUNK_H___
+#define ___CHUNK_H___
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "block.h"
+
+typedef struct {
+    Block *blocks;
+    size_t num_blocks;
+    float *mesh;
+    size_t mesh_size;
+} Chunk;
+
+Chunk chunk_new();
+void chunk_add_block(Chunk *chunk, Block block);
+void chunk_build_mesh(Chunk *chunk);
+
+#endif
---
+++ src/chunk.c
@@ -0,0 +1,14 @@
+#include "../include/chunk.h"
+
+Chunk chunk_new() {
+    Chunk chunk;
+    chunk.num_blocks = 0;
+}
+
+void chunk_add_block(Chunk *chunk, Block block) {
+
+}
+
+void chunk_build_mesh(Chunk *chunk) {
+
+}
